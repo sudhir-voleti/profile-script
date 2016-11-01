2dd250e73f3950956fec6021300a9bacfe135a02
sudhir-voleti <sudhir_voleti@isb.edu>
2016-11-01 17:29:11 +0530

    edited test profile
    yay

--- test profile.R
+++ test profile.R
@@ -1,3 +1,19 @@
+## text cleaning func
+
+text.clean = function(x)                    # text data
+{ require("tm")
+  x  =  gsub("<.*?>", " ", x)               # regex for removing HTML tags
+  x  =  iconv(x, "latin1", "ASCII", sub="") # Keep only ASCII characters
+  x  =  gsub("[^[:alnum:]]", " ", x)        # keep only alpha numeric 
+  x  =  tolower(x)                          # convert to lower case characters
+  x  =  removeNumbers(x)                    # removing numbers
+  x  =  stripWhitespace(x)                  # removing white space
+  x  =  gsub("^\\s+|\\s+$", "", x)          # remove leading and trailing white space
+  return(x)
+}
+
+
+
 data = data.frame(id = 1:length(temp.text), text = temp.text, stringsAsFactors = F)
 # dim(data)
 
